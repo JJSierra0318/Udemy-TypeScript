bfcdc5c243366be75cddfdb31e8dfce1111fff2e
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-01-23 09:22:25 -0500

    Variables

---
+++ Types/variables.ts
@@ -0,0 +1,16 @@
+//Basic Types
+let movieTitle: string = "LoTR"
+movieTitle = "Arrival"
+
+let numCatLives: number = 9
+numCatLives = 0
+
+let gameOver: boolean = false
+gameOver = true
+
+//Inference
+let tvShow = "CW"
+
+//any
+let thing: any = 0
+thing = "asd"
