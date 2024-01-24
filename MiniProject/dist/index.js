81889eeac31a026c9b9e4179e0fe715b5e5ba88a
unknown <juanjosesierram@gmail.com>
2024-01-24 12:59:58 -0500

    MiniProject - Part 2

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,8 +1,46 @@
 "use strict";
 //! The items Exists or exists
 const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
 //Run only if btn exists
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => alert("CLICKED!"));
+//btn?.addEventListenetr()
+const todos = readTodos();
+todos.forEach(createTodo);
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = "";
+});
 //It say it is a string because I say so
 let mystery = "Hello World";
 const numChars = mystery.length;
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,8 +1,59 @@
+interface Todo {
+    text: string,
+    completed: boolean
+}
+
 //! The items Exists or exists
-const btn = document.getElementById("btn")!
+const btn = document.getElementById("btn")! as HTMLButtonElement
+const input = document.getElementById("todoinput")! as HTMLInputElement
+const form = document.querySelector("form")!
+const list = document.getElementById("todolist")!
 
 //Run only if btn exists
-btn?.addEventListener("click", () => alert("CLICKED!"))
+//btn?.addEventListenetr()
+
+const todos: Todo[] = readTodos()
+todos.forEach(createTodo)
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function readTodos(): Todo[] {
+    const todosJSON = localStorage.getItem("todos")
+    if (todosJSON === null) return []
+    return JSON.parse(todosJSON);
+}
+
+function createTodo(todo: Todo) {
+    const newLI = document.createElement("li")
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.checked = todo.completed
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked
+        saveTodos()
+    })
+
+    newLI.append(todo.text)
+    newLI.append(checkbox)
+    list.append(newLI)
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    }
+
+    createTodo(newTodo)
+    todos.push(newTodo)
+    
+    saveTodos()
+    input.value = ""
+    
+})
 
 //It say it is a string because I say so
 let mystery: unknown = "Hello World"
